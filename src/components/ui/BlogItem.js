cad293f04f182946a619bc399ce233e0a81358a1
dvitvitskiy.pro <dvitvitskiy.pro@gmail.com>
2017-11-15 21:46:02 +0300

    react_04 fix+

--- src/components/ui/BlogItem.js
+++ src/components/ui/BlogItem.js
@@ -5,34 +5,29 @@ import TextBox    from './TextBox';
 import PropTypes  from 'prop-types';
 import { Grid, Segment } from 'semantic-ui-react';
 
-class BlogItem extends React.Component {
-  render() {
-    const { id, like, dislike, title, body, image, meta, likeFunc, dislikeFunc } = this.props;
-    return (
-      <div>
-        <hr />
-        <Grid columns={3} relaxed>
-          <Grid.Column>
-            <Segment basic>
-              <TextBox id={id} title={title} body={body} meta={meta} />
-            </Segment>
-          </Grid.Column>
-          <Grid.Column>
-            <Segment basic>
-              <Image {...image} />
-            </Segment>
-          </Grid.Column>
-        </Grid>
-        <Like
-          like={like}
-          dislike={dislike}
-          likeFunc={() => likeFunc(id)}
-          dislikeFunc={() => dislikeFunc(id)}
-        />
-      </div>
-    );
-  }
-}
+const BlogItem = ({ id, like, dislike, title, body, image, meta, likeFunc, dislikeFunc }) => (
+  <div>
+    <hr />
+    <Grid columns={3} relaxed>
+      <Grid.Column>
+        <Segment basic>
+          <TextBox id={id} title={title} body={body} meta={meta} />
+        </Segment>
+      </Grid.Column>
+      <Grid.Column>
+        <Segment basic>
+          <Image {...image} />
+        </Segment>
+      </Grid.Column>
+    </Grid>
+    <Like
+      like={like}
+      dislike={dislike}
+      likeFunc={() => likeFunc(id)}
+      dislikeFunc={() => dislikeFunc(id)}
+    />
+  </div>
+);
 BlogItem.defaultProps = {};
 BlogItem.propTypes = {
   id:          PropTypes.number,
