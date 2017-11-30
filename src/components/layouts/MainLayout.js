7433dadc9fafc09c8383a76cacf38645cccc3ea3
dvitvitskiy.pro <dvitvitskiy.pro@gmail.com>
2017-11-30 08:51:39 +0300

    react_05-2++

--- src/components/layouts/MainLayout.js
+++ src/components/layouts/MainLayout.js
@@ -1,30 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'components/elements/Link';
-import { Header, Segment, Container } from 'semantic-ui-react';
+import { Header, Segment, Input, Icon } from 'semantic-ui-react';
 import { rootPath, aboutPath } from 'helpers/routes';
 
 const MainLayout = ({children}) => (
-  <Container>
+  <div>
     <Logo />
     {children}
     <Footer />
-  </Container>
+  </div>
 );
 
 const Logo = () => (
   <Segment>
     <Header>
-      <div className="ui horizontal segments">
-        <div className="ui segment">
-          <p>
-            <Link to={ rootPath } >Blog</Link>
-          </p>
-        </div>
-        <div className="ui segment">
-          <p>
-            <Link to={ aboutPath } >About</Link>
-          </p>
+      <div className="ui attached stackable menu">
+        <div className="ui container">
+          <Link className="item home icon" to={ rootPath } >Home</Link>
+          <Link className="item info icon" to={ aboutPath } >About</Link>
+          <div className="ui simple dropdown item">
+            Sample
+            <i className="dropdown icon"></i>
+            <div className="menu">
+              <a className="item"><i className="edit icon"></i> Edit Profile</a>
+              <a className="item"><i className="globe icon"></i> Choose Language</a>
+              <a className="item"><i className="settings icon"></i> Account Settings</a>
+            </div>
+          </div>
+          <div className="right item">
+            <div className="ui input">
+              <input type="text" placeholder="Search..." />
+            </div>
+            <Input icon placeholder='Search...'>
+              <Icon name='search' />
+            </Input>
+          </div>
         </div>
       </div>
     </Header>
--- src/components/ui/BlogItem.js
+++ src/components/ui/BlogItem.js
@@ -1,26 +1,24 @@
-import React             from 'react';
-import Like              from 'components/ui/Like';
-import Image             from 'components/ui/Image';
-import TextBox           from 'components/ui/TextBox';
-import Link              from 'components/elements/Link';
-import PropTypes         from 'prop-types';
-import { postPath }      from 'helpers/routes';
-import { Grid, Segment } from 'semantic-ui-react';
+import React      from 'react';
+import Like       from 'components/ui/Like';
+import Image      from 'components/ui/Image';
+import TextBox    from 'components/ui/TextBox';
+import Link       from 'components/elements/Link';
+import PropTypes  from 'prop-types';
+import {postPath} from 'helpers/routes';
 
 const BlogItem = ({ id, like, dislike, title, body, image, meta, likeFunc, dislikeFunc }) => (
   <div className="ui raised link card">
     <div className="content">
+      <Link to={postPath(id)}>{title}</Link>
       <TextBox body={body} meta={meta} />
       <div className="description">
-        <p>
-          <Image {...image} />
-          <Like
-            like={like}
-            dislike={dislike}
-            likeFunc={() => likeFunc(id)}
-            dislikeFunc={() => dislikeFunc(id)}
-          />
-        </p>
+        <Image {...image} />
+        <Like
+          like={like}
+          dislike={dislike}
+          likeFunc={() => likeFunc(id)}
+          dislikeFunc={() => dislikeFunc(id)}
+        />
       </div>
     </div>
   </div>
